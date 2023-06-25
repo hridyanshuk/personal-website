2524a456df24f3f0bfa29b002f6f40ce7b4af07f
Hridyanshu Kumar <hridyanshukumar@gmail.com>
2023-06-25 14:27:32 +0530

    Commit 1: Home page

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <div className="leftsidebar">
+
+            </div>
+            <div className="main">
+                <h1>Hridyanshu Kumar's personal website</h1>
+                <div className='about'>
+                    <h2>About</h2>
+                    <hr />
+                    <p>
+                        My name is <b>Hridyanshu Kumar</b> and I am a student of <b>IIT Guwahati</b>.
+                        I am doing my Major in <b>Computer Science and Engineering</b> and 
+                        am currently in my 4th year.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
